9cec4d03103aef3bf076f22fbe059593a8bcfb0d
Paul Yoder <paulyoder@gmail.com>
2013-05-22 13:10:34 -0500

    added Card.get_history

--- lib/leankitkanban/card.rb
+++ lib/leankitkanban/card.rb
@@ -6,6 +6,7 @@ module LeanKitKanban
     DELETE_CARD   = "/Board/{boardID}/DeleteCard/{cardID}"
     FIND_CARD     = "/Board/{boardID}/GetCard/{cardID}"
     FIND_CARD_EXT = "/Board/{boardID}/GetCardByExternalId/{externalID}"
+    CARD_HISTORY  = "/Card/History/{boardID}/{cardID}"
 
     def self.delete(board_id, card_id)
       api_call = DELETE_CARD.gsub("{boardID}", board_id.to_s).gsub("{cardID}", card_id.to_s)
@@ -21,5 +22,10 @@ module LeanKitKanban
       api_call = FIND_CARD_EXT.gsub("{boardID}", board_id.to_s).gsub("{externalID}", external_id.to_s)
       get(api_call)
     end
+
+    def self.get_history(board_id, card_id)
+      api_call = CARD_HISTORY.gsub("{boardID}", board_id.to_s).gsub("{cardID}", card_id.to_s)
+      get(api_call)
+    end 
   end
 end
